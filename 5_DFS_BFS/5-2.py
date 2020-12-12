d95fd1c078be8d3e1511e8680a2e2b4a87cfa6a9
woonijung <jdu2038@gmail.com>
2020-12-12 23:28:38 +0900

    DFS 예제 풀이(p.143~p.148)

---
+++ 5_DFS_BFS/5-2.py
@@ -0,0 +1,30 @@
+# 예제 5-2. 음료수 얼려 먹기
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    # 주어진 범위가 벗어났을 경우에는 즉시 종료
+    if x<= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상,하,좌,우의 위치도 모두 재귀적으로 호출
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            result += 1
+
+print(result)
